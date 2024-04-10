79738c14e1c90adb611eda8f30bc5006f38d141e
emerald12307 <160066834+emerald12307@users.noreply.github.com>
2024-04-10 12:45:37 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+//Phrase 1
+document.getElementById("btn1").addEventListener("click", completePhrase1);
+function completePhrase1() {
+  alert("Make a long story short! \nMeaning: tell something briefly");
+}
+
+//Phrase 2
+document.getElementById("btn2").addEventListener("click", completePhrase2);
+function completePhrase2() {
+  alert("Get your act together! \nMeaning: Work better or leave ");
+}
+
+//Phrase 3
+document.getElementById("btn3").addEventListener("click", completePhrase3);
+function completePhrase3() {
+  alert("Break a leg! \nMeaning: Good luck");
+}
+
+// Phrase 4
+document.getElementById("btn4").addEventListener("click", completePhrase4);
+function completePhrase4() {
+  alert("Call it a day! \nMeaning: Stop working on something");
+}
+
+//Phrase 5
+document.getElementById("btn5").addEventListener("click", completePhrase5);
+function completePhrase5() {
+  alert("No pain, no gain! \nMeaning: You have to work for what you want");
+}
